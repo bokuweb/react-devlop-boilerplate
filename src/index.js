768f84257a52abed50bcc7ef123891c2e7c22e78
bokuweb <bokuweb12@gmail.com>
2016-02-12 10:07:32 +0900

    Add aslant

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 export default class HelloWorld extends Component {
   render() {
-    return(
-      <h1 style={{color: 'red'}}>Hello, world</h1>
+    return (
+      <h1 style={{ color: 'red' }}>Hello, world</h1>
     );
   }
 }
